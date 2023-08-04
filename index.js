8f4adb131c65bec5e85c3c846ded9f77bda493d3
Sainath Kommagoni <jsreactdevsai@gmail.com>
2023-08-04 23:57:29 +0530

    added env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5500;
